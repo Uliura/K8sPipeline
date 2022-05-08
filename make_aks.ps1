f77f9ca630a003f31b32c30f25a332fd32215e98
Evgen Uliura <testuser@ukr.net>
2022-05-08 18:38:31 +0300

    ok

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,7 +1,4 @@
-Write-Host "$env:TENANT"
-Write-Host "$env:APPID"
-Write-Host "$env:MY_MAPPED_ENV_VAR"
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:APPID, $env:PASSWORD
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:APPID $env:PASSWORD
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
 New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
