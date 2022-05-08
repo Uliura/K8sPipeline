8d4a3b22b8b8a171ba625f7554e7965f5f37cd3a
Evgen Uliura <testuser@ukr.net>
2022-05-08 19:03:19 +0300

    new

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,4 +1,4 @@
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential ("$env:APPID",$env:PASSWORD)
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential ("$env:APPID","$env:PASSWORD")
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
 New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
