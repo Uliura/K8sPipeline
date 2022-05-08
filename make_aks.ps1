4a634dcce5cae1d864d5c25384aa379fbbb88562
Evgen Uliura <testuser@ukr.net>
2022-05-08 16:11:21 +0300

    variable fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,7 +1,4 @@
-Write-Host "$env:TENANT"
-Write-Host "$env:APPID"
-Write-Host "$(secretId)"
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AppId, $secretId
-Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:APPID, $env:SECRETID
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
 New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
