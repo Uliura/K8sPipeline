c21839992ae1ff824d70230847bb534c1bae5a4d
Evgen Uliura <testuser@ukr.net>
2022-05-08 16:17:46 +0300

    ok

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,6 +1,6 @@
 Write-Host "$env:TENANT"
 Write-Host "$env:APPID"
-Write-Host "$env:SECRETID"
+Write-Host "$env:MY_MAPPED_ENV_VAR"
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:APPID, $env:SECRETID
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
