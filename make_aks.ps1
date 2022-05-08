5fdd050fadc3eabdfa38898b04617a99d14b3874
Evgen Uliura <testuser@ukr.net>
2022-05-08 20:29:02 +0300

    asd

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,3 +1,3 @@
 Connect-AzAccount
-New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
+New-AzResourceGroup -Name $env:resourcegroupname -Location $(azurelocation) -Force
 New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
