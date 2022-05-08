28f079f581976956afebc72ada53ba49ab1fbe2f
Evgen Uliura <testuser@ukr.net>
2022-05-08 15:58:00 +0300

    ok

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,7 +1,7 @@
 Write-Host "$tenant"
 Write-Host "$appid"
 Write-Host "$secretId"
-#$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SP.AppId, $SP.secretId
-#Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $SP.tenant
-#New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
-#New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AppId, $secretId
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenant
+New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
+New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
