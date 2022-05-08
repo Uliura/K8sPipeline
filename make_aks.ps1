9fcf5e6876a2c649eda035fb27ca00aec4f1830f
Evgen Uliura <testuser@ukr.net>
2022-05-08 16:12:38 +0300

    check secret

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,3 +1,6 @@
+Write-Host "$env:TENANT"
+Write-Host "$env:APPID"
+Write-Host "$env:SECRETID"
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:APPID, $env:SECRETID
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
