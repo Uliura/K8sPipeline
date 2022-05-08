4bc3fdd13d6e1173dac4650ed4d75aac0366c1ea
Evgen Uliura <testuser@ukr.net>
2022-05-08 15:40:35 +0300

    change pwsh script

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,3 +1,6 @@
+Write-Host $SP.tenant
+Write-Host $SP.AppId
+Write-Host $SP.secretId
 $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SP.AppId, $SP.secretId
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $SP.tenant
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
