2756150f8cff109d85544438655426789275f083
Evgen Uliura <testuser@ukr.net>
2022-05-08 19:11:50 +0300

    sda

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,4 +1,4 @@
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential ("7aceb566-5abd-4f7e-8c08-48b1c30f7222",9b2ed638-55b2-4f32-8dc0-c9195df712cf)
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "7aceb566-5abd-4f7e-8c08-48b1c30f7222", 9b2ed638-55b2-4f32-8dc0-c9195df712cf)
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $env:TENANT
 New-AzResourceGroup -Name $(resourcegroupname) -Location $(azurelocation) -Force
 New-AzAksCluster -ResourceGroupName $(resourcegroupname) -Name $(aksclustename) -NodeCount 1 -GenerateSshKey -Force
