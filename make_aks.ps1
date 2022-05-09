5282bd3124c39a486c576cca91f86a8f111cf00e
Evgen Uliura <testuser@ukr.net>
2022-05-09 12:30:18 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,1 +1,1 @@
-New-AzAksCluster -ResourceGroupName AzureDevOps -Name xzklaster -NodeCount 1 -Location eastus -GenerateSshKey -Force
+New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name xzklaster -NodeCount 1 -Location eastus -GenerateSshKey -Force
