6b68d4c03d5d73805aa50de4881c691cbd1731c5
Evgen Uliura <testuser@ukr.net>
2022-05-09 20:12:10 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,2 +1,2 @@
 Connect-AzAccount -UseDeviceAuthentication
-New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Confirm:$false
+New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -NodeVmSize standard_D2ads_v5 -Force -Confirm:$false
