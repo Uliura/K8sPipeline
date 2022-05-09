a1e6db3fc4ceca25da31ef47706d07700ed86313
Evgen Uliura <testuser@ukr.net>
2022-05-09 18:21:01 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,2 +1,4 @@
+Install-Module -Name Az -AllowClobber -Force
+Import-Module Az
 New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Debug
 
