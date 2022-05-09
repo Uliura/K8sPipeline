2a58f311e3302980e32b2b2941338df830b9088f
Evgen Uliura <testuser@ukr.net>
2022-05-09 19:48:31 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,1 +1,3 @@
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $env:servicePrincipalId,$env:servicePrincipalKey
+Connect-AzAccount -Credential $Credential -Tenant $env:tenantId
 New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Confirm:$false
