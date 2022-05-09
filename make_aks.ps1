c4b9e146c34dece18bd75b3b48014e829fc4fea6
Evgen Uliura <testuser@ukr.net>
2022-05-09 13:58:14 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,4 +1,2 @@
-$VerbosePreference="Continue"
-Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
 New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -GenerateSshKey -NodeVmSize standard_D2ads_v5 -Force
 
