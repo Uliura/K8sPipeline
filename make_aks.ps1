0a5377f0a83e44e6a609e74d60a92fcfe011292a
Evgen Uliura <testuser@ukr.net>
2022-05-09 19:56:39 +0300

    fix

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,4 +1,5 @@
-$secPassword = ConvertTo-SecureString $env:servicePrincipalKey -AsPlainText -Force
+[string][ValidateNotNullOrEmpty()] $secPassword = $env:servicePrincipalKey
+$secPassword = ConvertTo-SecureString $secPassword -AsPlainText -Force
 $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $env:servicePrincipalId,$secPassword
 Connect-AzAccount -Credential $Credential -Tenant $env:tenantId
 New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Confirm:$false
