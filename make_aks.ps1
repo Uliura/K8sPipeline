486945b2ce34bb13acf281edb040b5469b5f892a
Evgen Uliura <testuser@ukr.net>
2022-05-09 19:39:35 +0300

    jj

--- make_aks.ps1
+++ make_aks.ps1
@@ -1,2 +1,1 @@
-New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Debug
-
+New-AzAksCluster -ResourceGroupName $env:RESOURCEGROUPNAME -Name $env:AKSCLUSTERNAME -NodeCount 1 -Location $env:AZURELOCATION -SshKeyValue '/root/.ssh/id_rsa' -NodeVmSize standard_D2ads_v5 -Force -Confirm:$false
