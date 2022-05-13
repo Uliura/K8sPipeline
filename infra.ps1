f74e7af27cd3fa76f617d19ec5c16e5fd9aee185
Evgen Uliura <testuser@ukr.net>
2022-05-13 17:08:34 +0300

    fix

--- infra.ps1
+++ infra.ps1
@@ -6,7 +6,7 @@ $AksLocation = "eastus"
 $AKSCheck = Get-AzAksCluster -Name $AKSName -ResourceGroupName $RGName -ErrorAction SilentlyContinue
 Write-Host "Check = $AKSCheck"
 if($null -eq $AKSCheck){
-    New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -Force
+    New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -NodeVmSize -Force
     Write-Host "Error occurred: $_"
 }
 else{
