145778df5af23206448a368302e9e224bb568ac2
Evgen Uliura <testuser@ukr.net>
2022-05-13 14:11:31 +0300

    fix

--- infra.ps1
+++ infra.ps1
@@ -6,7 +6,7 @@ $AksLocation = "eastus"
 $AKSCheck = Get-AzAksCluster -Name $AKSName -ResourceGroupName $RGName -ErrorAction SilentlyContinue
 
 if($AKSCheck -eq $null){
-    New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -SshKeyValue $env:System.WorkFolder -Force
+    New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -Force
 
 }
 else{
