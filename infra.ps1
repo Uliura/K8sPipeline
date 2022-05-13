2e68d368a52d8991726f0cc002fa0a3c34531c81
Evgen Uliura <testuser@ukr.net>
2022-05-13 17:27:00 +0300

    fix

--- infra.ps1
+++ infra.ps1
@@ -8,7 +8,6 @@ $AKSCheck = Get-AzAksCluster -Name $AKSName -ResourceGroupName $RGName -ErrorAct
 Write-Host "Check = $AKSCheck"
 if($null -eq $AKSCheck) {
     New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -NodeVmSize $NodeSize -Force
-    Write-Host "Error occurred: $_"
 }
 else {
     Write-Host "Kubernetes cluster $AKSName already exist"
