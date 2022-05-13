3a54351f26405d15d3680a87cea8f797dba57f87
Evgen Uliura <testuser@ukr.net>
2022-05-13 13:08:43 +0300

    fix

---
+++ infra.ps1
@@ -0,0 +1,14 @@
+$AKSName = "xzcluster"
+$ResourceGroupName = "AzureDevOps"
+$Nodes = "1"
+$AksLocation = "eastus"
+
+$AKSName = Get-AzAksCluster -Name $AKSName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
+
+if($AKSName -eq $null){
+    New-AzAksCluster -ResourceGroupName $ResourceGroupName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -SshKeyValue $(System.ArtifactsDirectory) -Force
+
+}
+else{
+    Write-Host "Kubernetes cluster $AKSName already exist"
+}
