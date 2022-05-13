7714371cdf82eb1e4fa6f948dddfc3c011fdca22
Evgen Uliura <testuser@ukr.net>
2022-05-13 13:48:41 +0300

    fix

--- infra.ps1
+++ infra.ps1
@@ -1,8 +1,11 @@
-Write-Host "$AKSName, $RGName, $Nodes , $AksLocation"
+$AKSName = "xzcluster"
+$RGName = "AzureDevOps"
+$Nodes = "1"
+$AksLocation = "eastus"
 
-$AKSName = Get-AzAksCluster -Name $AKSName -ResourceGroupName $RGName -ErrorAction SilentlyContinue
+$AKSCheck = Get-AzAksCluster -Name $AKSName -ResourceGroupName $RGName -ErrorAction SilentlyContinue
 
-if($AKSName -eq $null){
+if($AKSCheck -eq $null){
     New-AzAksCluster -ResourceGroupName $RGName -Name $AKSName -NodeCount $Nodes -Location $AksLocation -GenerateSshKey -SshKeyValue $env:System.WorkFolder -Force
 
 }
